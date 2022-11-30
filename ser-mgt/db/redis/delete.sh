9d5de1d363335950882a24b36caca39e83b390dc
kim-yeji <2214yjgm@gmail.com>
2022-12-01 00:37:24 +0900

    Add Redis Cluster Manifest files

--- ser-mgt/db/redis/delete.sh
+++ ser-mgt/db/redis/delete.sh
@@ -1,6 +1,6 @@
 
-kubectl delete -f redis.yaml
-kubectl delete -f redis-config.yaml
-kubectl delete -n redis -f redis-statefulset.yaml
-kubectl delete -n redis -f redis-svc.yaml
 kubectl delete ns redis
+kubectl delete -f redis-config.yaml
+kubectl delete -f redis-statefulset.yaml
+kubectl delete -f redis-svc.yaml
+kubectl delete pvc --all
